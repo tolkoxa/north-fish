5f27c418930cf86048e1c1082833f8ddb6ecf34e
tolkoxa <tolkoxa@ya.ru>
2020-12-16 23:16:11 +0300

    Сделал вёрстку 'второй ветки' и прописал скрипты первых блоков

--- js/fish.js
+++ js/fish.js
@@ -4,6 +4,8 @@ class Seafood {
         this._init();
         this.allCategories;
         this.allGoods;
+        this.orderGoods = [];
+        this.dinnerType;
         document.getElementById('buttons').addEventListener('click', (evt) => {
             evt.preventDefault();
             let e = evt.target
@@ -61,7 +63,6 @@ class Seafood {
         this.screen.insertAdjacentHTML('beforeend', mainStr);
 
         document.getElementById('catNext').addEventListener('click', () => {
-            console.log('Дальшеееее');
             let checkboxArr = document.querySelectorAll('input[type=checkbox]:checked');
             this.chooseCategories(checkboxArr, this.allGoods);
         })
@@ -69,16 +70,15 @@ class Seafood {
     }
 
     chooseCategories(allCats, allGoods) {
-        let strFirstCode = `<section class="category__style">`;
+        let strFirstCode = `<section class="category__style" id="orderitems">`;
         let strMainCode = '';
         let strLastCode = `
         <div class="category__common-button">
-                    <button class="category__last-btn">К заказу</button>
+                    <button class="category__last-btn" id="order">К заказу</button>
                 </div>
             </section>`;
 
         allCats.forEach((e) => {
-            // console.log(this.allCategories[e.id - 1].name);
             strMainCode = strMainCode + `<div class="category__item">
             <p class="category__title">${this.allCategories[e.id - 1].name}</p>`
             allGoods.forEach((index) => {
@@ -98,7 +98,7 @@ class Seafood {
                         </div>
                         <p class="category__desc">${index.desc}</p>
                         <div class="category__button">
-                            <button class="category__btn">Добавить в заказ</button>
+                            <button class="category__btn" name="ordergood" data-good="${index.good_id}">Добавить в заказ</button>
                         </div>
                     </div>
                     `
@@ -110,11 +110,145 @@ class Seafood {
         let strAllCode = strFirstCode + strMainCode + strLastCode;
         this.screen.innerHTML = '';
         this.screen.insertAdjacentHTML('beforeend', strAllCode);
+
+        document.getElementById('orderitems').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            let e = evt.target
+
+            if (e.name === 'ordergood') {
+                this.orderGoods.push(e.dataset.good);
+            }
+        });
+
+        document.getElementById('order').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.contact();
+        })
     }
 
+    contact() {
+        this.screen.innerHTML = '';
+        let strMainCode = `
+        <section class="form__style">
+            <p class="form__title">Дополнительная информация</p>
+            <div class="form__container">
+                <p class="form__lable">Как к вам можно обращаться?</p>
+                <input class="form__input" type="text" placeholder="Как к вам можно обращаться?" id="name">
+                <p class="form__lable">Ваш телефон?</p>
+                <input class="form__input" type="text" placeholder="Ваш телефон" id="tel">
+                <p class="form__lable">Адрес доставки?</p>
+                <input class="form__input" type="text" placeholder="Адрес доставки" id="address">
+                <p class="form__lable">Дата и удобное время доставки?</p>
+                <input class="form__input" type="text" placeholder="Дата и удобное время доставки" id="deldate">
+                <p class="form__lable">Для уточнения заказа нам необходимо вам позвонить или написать <br>(если не сможете ответить). <br>Когда это лучше всего сделать?</p>
+                <input class="form__input" type="text" placeholder="Дата и удобное время" id="calldate">
+                <p class="form__lable">Кто порекомендовал к нам обратиться?</p>
+                <input class="form__input" type="text" placeholder="Кто порекомендовал" id="recom">
+            </div>
+            <div class="form__button">
+                <button class="form__btn" id="rightorder">Всё верно. Жду заказ</button>
+            </div>
+        </section>`;
+        this.screen.insertAdjacentHTML('beforeend', strMainCode);
+
+        let dataOrder = 'Заказ:';
+        let i = false;
+        this.orderGoods.forEach((index) => {
+            (i) ? dataOrder = `${dataOrder},`: dataOrder = dataOrder, i = true;
+            dataOrder = `${dataOrder} ${this.allGoods.find(x => x.good_id == index).name}`;
+        });
+
+        let clInfo = '';
+        document.getElementById('rightorder').addEventListener('click', () => {
+            clInfo = `${clInfo}Имя: ${document.getElementById('name').value} **** Телефон: ${document.getElementById('tel').value} **** Адрес доставки: ${document.getElementById('address').value} **** Дата и время доставки: ${document.getElementById('deldate').value} **** Лучшее время для звонка: ${document.getElementById('calldate').value} **** Кто порекомендовал: ${document.getElementById('recom').value}`;
+            console.log(dataOrder);
+            console.log(clInfo);
+            this.datasend(dataOrder, clInfo, document.getElementById('name').value);
+        })
+    }
+    datasend(zakaz, klient, name) {
+        this.screen.innerHTML = '';
+        (name != '') ? name = name + ',': name = name;
+        let strMainCode = `
+            <section class="order__style">
+                <div class="order__final">
+                    <p class="order__name">${name}</p>
+                    <p class="order__thnx">спасибо!</p>
+                    <p class="order__text">Ваш заказ поступил Михаилу. В ближайшее время он позвонит/напишет Вам для подтверждения заказа.</p>
+                    <div class="order__img">
+                        <img src="img/white-fish.png" alt="">
+                        <img src="img/white-fish.png" alt="">
+                        <img src="img/white-fish.png" alt="">
+                        <img src="img/white-fish.png" alt="">
+                    </div>
+                </div>
+                <div class="cover__title">
+                    <h1 class="cover__title-h1">Рекомендую!</h1>
+                    <h2 class="cover__title-h2">Морепродукты с Севера</h2>
+                </div>
+                <img class="cover__img" src="img/cover-bg-img.png" alt="Морепродукты с Севера" width="100%" height="auto">
+                <p class="order__ref-link">Скопируйте ссылку, чтобы рекомендовать нас своим</p>
+            </section>
+        `;
+        this.screen.insertAdjacentHTML('beforeend', strMainCode);
+    }
     needHelp() {
-        console.log('Подскажите111');
+        let strMainCode = `
+            <section class="event__style">
+                <div class="event__container" id="dinner">
+                    <p class="event__text">Для какого события выбираете морепродукты?</p>
+                    <p class="event__item" name="dinner" data-dinner="1">Обычный ужин дома</p>
+                    <p class="event__item" name="dinner" data-dinner="2">Праздничный ужин</p>
+                    <p class="event__item" name="dinner" data-dinner="3">В подарок</p>
+                </div>
+            </section>
+        `;
+        this.screen.innerHTML = '';
+        this.screen.insertAdjacentHTML('beforeend', strMainCode);
+        document.getElementById('dinner').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            let e = evt.target;
+            this.dinnerType = e.dataset.dinner;
+            this.guestsBudget();
+        })
+    }
+
+    guestsBudget() {
+        let strMainCode = `
+            <section class="clarify__style">
+                <div class="clarify__inputs">
+                    <div class="clarify__input-label">
+                        <p class="clarify__text">Сколько планируется гостей?</p>
+                        <input class="clarify__input" type="text" placeholder="Сколько планируется гостей">
+                    </div>
+                    <div class="clarify__input-label">
+                        <p class="clarify__text">Какой планируете бюджет?</p>
+                        <input class="clarify__input" type="text" placeholder="Какой планируете бюджет">
+                    </div>
+                </div>
+                <div class="clarify__checks">
+                    <div class="clarify__check-label">
+                        <input class="clarify__checkbox" type="checkbox" name="" id="suprise">
+                        <label class="clarify__label" for="suprise">Удивить гостей <br>(подберём что–то необычное)</label>
+                    </div>
+                    <div class="clarify__check-label">
+                        <input class="clarify__checkbox" type="checkbox" name="" id="gift">
+                        <label class="clarify__label" for="gift">Подарочная упаковка</label>
+                    </div>
+                </div>
+                <div class="clarify__button">
+                    <button class="clarify__btn" id="questsbudget">Дальше</button>
+                </div>
+            </section>
+        `;
+
+        this.screen.innerHTML = '';
+        this.screen.insertAdjacentHTML('beforeend', strMainCode);
+
+        document.getElementById('questsbudget').addEventListener('click', () => {
+            this.contact();
+        })
     }
 
 }
-let chText = new Seafood();
+let Fish = new Seafood();
